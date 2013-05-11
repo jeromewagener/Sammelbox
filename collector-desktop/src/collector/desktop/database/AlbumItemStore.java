bff7b7874c1eba6537e69c6ae571b55152b7d2f8
paul bicheler <paul.bicheler@gmx.net>
2013-05-12 00:41:02 +0200

    Since the albumItemstore is also used to present queries, the existing isOutdated check does not work since it only checks against database changes.

--- collector-desktop/src/collector/desktop/database/AlbumItemStore.java
+++ collector-desktop/src/collector/desktop/database/AlbumItemStore.java
@@ -9,20 +9,13 @@ public class AlbumItemStore {
 	
 	private static List<AlbumItem> albumItems = new ArrayList<AlbumItem>();
 	private static String currentAlbumName = "";
-	private static long lastReinitalized = 0;
 	private static int stopIndex = DEFAULT_STOP_INDEX;
 	private static int previousStopIndex = DEFAULT_STOP_INDEX;
 	
 	public static void reinitializeStore(AlbumItemResultSet albumItemResultSet) {
 		
-		if (!isOutDated() && currentAlbumName.equals(albumItemResultSet.getAlbumName())) {
-			// Don't do anything when the store is not really outdated.
-			albumItemResultSet.close();
-			return;
-		}
 		currentAlbumName = albumItemResultSet.getAlbumName();
 		albumItems.clear();
-		lastReinitalized = System.currentTimeMillis();
 		stopIndex = DEFAULT_STOP_INDEX;
 		previousStopIndex = DEFAULT_STOP_INDEX;
 		
@@ -40,21 +33,6 @@ public class AlbumItemStore {
 		}
 		
 		albumItemResultSet.close();
-	}
-	
-	public static boolean isOutDated() {
-		// Use the database wrapper to determine when the database 
-		// was last changed in order to find out whether the cache is out-dated
-		long lastDatabaseChangeTimeStamp = DatabaseWrapper.getLastDatabaseChangeTimeStamp();
-		if (lastDatabaseChangeTimeStamp == -1) {
-			// On startup no changes have been done.
-			return true;
-			
-		}else if (AlbumItemStore.lastReinitalized <= lastDatabaseChangeTimeStamp) {
-			return true;
-		}
-		
-		return false;
 	}
 	
 	public static List<AlbumItem> getAllAlbumItems() {
