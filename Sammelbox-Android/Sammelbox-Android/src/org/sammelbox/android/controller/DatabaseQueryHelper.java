4e12011eba2f9e31f71f25189f97bc870c9a394b
jerome <jerome@jeromewagener.com>
2013-12-01 16:02:43 +0100

    Moved Android companion app prototype to own repository. This references #134

--- Sammelbox-Android/Sammelbox-Android/src/org/sammelbox/android/controller/DatabaseQueryHelper.java
+++
@@ -1,8 +0,0 @@
-package org.sammelbox.android.controller;
-
-public class DatabaseQueryHelper {
-	static boolean isSpecialField(String fieldName) {
-		return fieldName.equals("id") || fieldName.equals("content_version") ||
-				fieldName.equals("schema_version") || fieldName.equals("typeinfo");
-	}
-}
--- Sammelbox-Android/Sammelbox-Android/src/org/sammelbox/android/model/OptionType.java
+++
@@ -1,8 +0,0 @@
-package org.sammelbox.android.model;
-
-public enum OptionType {
-	YES,
-	NO,
-	UNKNOWN;
-}
-
